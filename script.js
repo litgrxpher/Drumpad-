7fc96e529c4b6f99cba189c35010812f5a2d60cf
Adharsh CS <84709739+litgrxpher@users.noreply.github.com>
2022-06-25 11:12:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function sound1(){
+    var snd = new Audio('tom-1.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+function sound2(){
+    var snd = new Audio('tom-2.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+function sound3(){
+    var snd = new Audio('tom-3.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+function sound4(){
+    var snd = new Audio('tom-4.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+function sound5(){
+    var snd = new Audio('crash.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+function sound6(){
+    var snd = new Audio('kick-bass.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
